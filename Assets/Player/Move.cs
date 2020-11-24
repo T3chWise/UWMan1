d085cd68c46f7cbbcfc2a0ffa652b6c25b24d356
dpark12300 <74691964+dpark12300@users.noreply.github.com>
2020-11-23 17:23:35 -0800

    Move Script

---
+++ Assets/Player/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehavior
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput=0, yInput=0;
+        yInput += Input.GetAxis("Vertical");
+        xInput += Input.GetAxis("Horizontal");
+        transform.Translate()
+    }
+}
